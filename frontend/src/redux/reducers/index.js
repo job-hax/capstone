af57e18cbd0167ff0656e921a5157f2a81424399
Yonglin Zhong <zhongyonglin@hotmail.com>
2019-09-21 00:36:54 -0700

    set up an api call example with integration of actual endpoint

--- frontend/src/redux/reducers/index.js
+++ frontend/src/redux/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
+import agreement from './agreementReducer';
 
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({ agreement });
 
 export default rootReducer;
--- frontend/webpack.config.dev.js
+++ frontend/webpack.config.dev.js
@@ -24,7 +24,7 @@ module.exports = {
   },
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.API_URL': JSON.stringify('http://localhost:3001')
+      'process.env.API_URL': JSON.stringify('https://backend.jobhax.com/api')
     }),
     new HtmlWebpackPlugin({
       template: 'src/index.html'
